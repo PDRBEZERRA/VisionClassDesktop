c5fef1c07a3f3fb6da24bcf053f20c8cf9ea7752
Pedro <pedroasbezerra05@gmail.com>
2025-10-08 16:35:49 -0300

    Colocando perfil no canto direito superior

--- src/main/java/br/com/undb/visionclass/visionclassdesktop/DashboardController.java
+++ src/main/java/br/com/undb/visionclass/visionclassdesktop/DashboardController.java
@@ -5,22 +5,34 @@ import br.com.undb.visionclass.visionclassdesktop.session.UserSession;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
 public class DashboardController {
 
-    // A variável welcomeLabel foi REMOVIDA daqui
-
     @FXML
     private BorderPane mainBorderPane;
 
+    // Variáveis para os novos componentes do cabeçalho
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    private ImageView userAvatar;
+
     @FXML
     public void initialize() {
-        // A lógica do welcomeLabel foi REMOVIDA daqui
+        // Busca o utilizador que fez o login
+        User loggedInUser = UserSession.getInstance().getLoggedInUser();
+        if (loggedInUser != null) {
+            // Define o nome do utilizador no Label do cabeçalho
+            userNameLabel.setText(loggedInUser.getNome());
+            // Futuramente, aqui poderia carregar a imagem do perfil a partir de loggedInUser.getFoto()
+        }
+
         // Carrega a tela inicial do dashboard por defeito
         loadCenterView("dashboard-home-view.fxml");
     }
@@ -37,7 +49,9 @@ public class DashboardController {
 
     @FXML
     private void onGerirTurmasClick() {
-        loadCenterView("turmas-view.fxml");
+        // Futuramente, isto irá carregar a tela de turmas
+        System.out.println("Botão 'Gerir Turmas' clicado!");
+        // loadCenterView("turmas-view.fxml");
     }
 
     @FXML
